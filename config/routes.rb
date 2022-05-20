0bb7f9b1bdd2752af4828901fe85cd38929d7a79
Raj Rana <rajrana22@uchicago.edu>
2022-05-20 03:41:31 +0000

    Started

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "users", :action => "index" })
 
   # User routes
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form" })
+
+  get("/user_sign_out", { :controller => "users", :action => "new_signout_form" })
+
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form" })
+
+  post("/verify_credentials", { :controller => "users", :action => "authenticate" })
 
   # CREATE
-  get("/insert_user_record", {:controller => "users", :action => "create" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
 
   # READ
-  get("/users", {:controller => "users", :action => "index"})
-  get("/users/:the_username", {:controller => "users", :action => "show"})
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:the_username", { :controller => "users", :action => "show" })
 
   # UPDATE
-  get("/update_user/:the_user_id", {:controller => "users", :action => "update" })
+  get("/update_user/:the_user_id", { :controller => "users", :action => "update" })
 
   # DELETE
-  get("/delete_user/:the_user_id", {:controller => "users", :action => "destroy"})
+  get("/delete_user/:the_user_id", { :controller => "users", :action => "destroy" })
 
   # Photo routes
 
@@ -23,15 +29,15 @@ Rails.application.routes.draw do
   get("/insert_photo_record", { :controller => "photos", :action => "create" })
 
   # READ
-  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos", { :controller => "photos", :action => "index" })
 
-  get("/photos/:the_photo_id", { :controller => "photos", :action => "show"})
+  get("/photos/:the_photo_id", { :controller => "photos", :action => "show" })
 
   # UPDATE
   get("/update_photo/:the_photo_id", { :controller => "photos", :action => "update" })
 
   # DELETE
-  get("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy"})
+  get("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy" })
 
   # Comment routes
 
@@ -40,6 +46,5 @@ Rails.application.routes.draw do
 
   # DELETE
 
-  get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy"})
-
+  get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy" })
 end
